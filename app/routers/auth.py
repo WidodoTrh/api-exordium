74fbbfb0cdb193f90d18542f69a79dc7c3941b73
Widodo <widodo.rahadi00@gmail.com>
2025-10-01 00:44:50 +0700

    [update] req.txt, update-error-jwt-decode

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -17,19 +17,22 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    expire = datetime.utcnow() + (expires_delta or timedelta(days=7))
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
 def get_current_user_from_cookie(request: Request, db: Session = Depends(get_db)):
     token = request.cookies.get("access_token")
+    # print("Token from cookie:", token)
     if not token:
         raise HTTPException(status_code=401, detail="Not authenticated")
     try:
         payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.ALGORITHM])
-        user_id = payload.get("sub")
-    except JWTError:
+        user_id = int(payload.get("sub")) # hmmmm ini dia permasalahan nya si anjing -________-
+        # print("Decoded payload:", payload)
+    except JWTError as e:
+        print("JWTError:", e)
         raise HTTPException(status_code=401, detail="Not authenticated")
     
     user = db.query(User).filter(User.id == user_id).first()
@@ -61,7 +64,7 @@ def google_login(token: str, db: Session = Depends(get_db)):
             db.add(user)
             db.commit()
             db.refresh(user)
-        access_token = create_access_token(data={"sub": user.id})
+        access_token = create_access_token(data={"sub": str(user.id)}) # tadi nya ini adalah integer karna ID. meanwhile kalo integer ga bisa di decode sama jwt. dan ternyata harus dirubah jd string
         res = JSONResponse(content = {
             "user": {
                 "id": user.id,
