7440a484445c7745e94ee3282cd8053375340cc8
WidodoTrh <widodo.rahadi00@gmail.com>
2025-10-08 16:42:57 +0700

    [add] user pict uri to db

--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,7 @@ class User(Base):
     google_id = Column(String(255), unique=True, index=True, nullable=True)
     email = Column(String(255), unique=True, index=True, nullable=False)
     name = Column(String(255), nullable=True)
+    pict_uri = Column(String(255), nullable=True)
     created_at = Column(DateTime, server_default=func.now())
 
     refresh_tokens = relationship("UserRefreshToken", back_populates="user", cascade="all, delete-orphan")
