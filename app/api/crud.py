a86c3c2e3d5bd49720b23c47687f3106aedce3de
WidodoTrh <widodo.rahadi00@gmail.com>
2025-10-13 17:01:15 +0700

    [update] db relation

--- app/api/crud.py
+++ app/api/crud.py
@@ -1,24 +1,21 @@
 from sqlalchemy.orm import Session
 from app.models import models
+from app.models.user import User
 from datetime import datetime
+
 
 def get_user_by_google_id(db: Session, google_id: str):
     return db.query(models.User).filter(models.User.google_id == google_id).first()
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
+        return db.query(User).filter(User.email == email).first()
 
-def create_user(db: Session, user_in: dict):
-    user = models.User(
-        google_id=user_in["google_id"],
-        email=user_in["email"],
-        name=user_in.get("name"),
-        avatar_url=user_in.get("avatar_url")
-    )
-    db.add(user)
+def create_user(db: Session, email: str, name: str, pict_uri: str):
+    new_user = User(email=email, name=name, pict_uri=pict_uri)
+    db.add(new_user)
     db.commit()
-    db.refresh(user)
-    return user
+    db.refresh(new_user)
+    return new_user
 
 def update_last_login(db: Session, user: models.User):
     user.last_login = datetime.utcnow()
@@ -26,3 +23,6 @@ def update_last_login(db: Session, user: models.User):
     db.commit()
     db.refresh(user)
     return user
+
+
+
